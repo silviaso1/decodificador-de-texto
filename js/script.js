eac6b20dff2f48ac04b4d0f7ced8621e97bbfc93
Silvia <silviasoares423@gmail.com>
2024-07-12 22:55:23 -0300

    upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+function criptografar() {
+    const texto = document.getElementById('texto').value;
+    let textoCriptografado = '';
+
+    for (let i = 0; i < texto.length; i++) {
+        const atual = texto[i];
+        switch (atual) {
+            case 'a':
+                textoCriptografado += 'ba';
+                break;
+            case 'e':
+                textoCriptografado += 'de';
+                break;
+            case 'i':
+                textoCriptografado += 'hi';
+                break;
+            case 'o':
+                textoCriptografado += 'no';
+                break;
+            case 'u':
+                textoCriptografado += 'tu';
+                break;
+            default:
+                textoCriptografado += atual;
+        }
+    }
+
+    document.getElementById('imgCriptografia').style.display = 'none';
+    document.getElementById('mensagem').style.display = 'none';
+    document.getElementById('textoNone').innerText = textoCriptografado;
+    document.getElementById('textoNone').style.marginTop = '45px';
+    document.getElementById('copiar').style.display = 'block';
+
+}
+
+function descriptografar() {
+    const texto = document.getElementById('texto').value;
+    let textoDescriptografado = texto;
+
+    textoDescriptografado = textoDescriptografado.replace(/ba/g, 'a');
+    textoDescriptografado = textoDescriptografado.replace(/de/g, 'e');
+    textoDescriptografado = textoDescriptografado.replace(/hi/g, 'i');
+    textoDescriptografado = textoDescriptografado.replace(/no/g, 'o');
+    textoDescriptografado = textoDescriptografado.replace(/tu/g, 'u');
+
+    document.getElementById('imgCriptografia').style.display = 'none';
+    document.getElementById('mensagem').innerText = textoDescriptografado;
+    document.getElementById('copiar').style.display = 'block';
+}
+
+function copiarTexto() {
+    const textoCriptografado = document.getElementById('mensagem').innerText;
+    navigator.clipboard.writeText(textoCriptografado);
+    
+}
+document.getElementById('criptografar').addEventListener('click', criptografar);
+document.getElementById('descriptografar').addEventListener('click', descriptografar);
+document.getElementById('copiar').addEventListener('click', copiarTexto);
